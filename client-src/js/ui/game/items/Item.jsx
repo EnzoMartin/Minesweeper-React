30a9b0c9df905e7fdf075f2e3693f2f6e1e3f2d2
Enzo Martin <enzo.r.martin@gmail.com>
2015-09-12 22:12:47 -0400

    Correcftly trigger won on last item

--- client-src/js/ui/game/items/Item.jsx
+++ client-src/js/ui/game/items/Item.jsx
@@ -9,7 +9,7 @@ var Item = React.createClass({
     },
     revealItem: function(){
         ItemsActions.revealItem(this.props.item);
-        if(ItemsStore.getRemaining().length <= 1){
+        if(ItemsStore.getRemaining().length < 1){
             PlayerActions.gameOver(true);
         }
     },
